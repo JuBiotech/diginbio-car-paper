7a4b00266ddbb86d9aee714a7f7d6fb7beb3f7dd
Michael Osthege <m.osthege@fz-juelich.de>
2021-07-21 15:47:51 +0200

    Extract data loading routines

---
+++ notebooks/dataloading.py
@@ -0,0 +1,28 @@
+import numpy
+import pandas
+import pathlib
+from robotools.transform import make_well_array
+
+
+DP_DATA = pathlib.Path("..") / "data"
+
+
+def get_layout():
+    df_layout = pandas.read_excel(DP_DATA / "WellDescription.xlsx").rename(columns={
+        "Well": "well",
+        "Content": "content",
+        "Product, mM": "product",
+    })
+    df_layout["well"] = make_well_array(R=8, C=12).flatten("C")
+    df_layout = df_layout.set_index("well")
+    return df_layout
+
+
+def read_absorbances(fp) -> pandas.DataFrame:
+    df = pandas.read_csv(fp, sep=";", index_col=0)
+    df.rename(inplace=True, columns={
+        c : f"{c[0]}{int(c[1:]):02d}"
+        for c in df.columns
+    })
+    df["time_hours"] = (df.index - df.index[0]) / 3600
+    return df.set_index("time_hours")
