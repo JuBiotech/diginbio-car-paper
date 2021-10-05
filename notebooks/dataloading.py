6336e38787602d3e4ca3511d4eb457b4b7a68d36
Michael Osthege <m.osthege@fz-juelich.de>
2021-10-05 18:42:57 +0200

    Add helper function for fetching files from the datadir

--- notebooks/dataloading.py
+++ notebooks/dataloading.py
@@ -8,6 +8,21 @@ from robotools.transform import make_well_array
 DP_DATA = pathlib.Path("..") / "data"
 
 
+def fetch_file(fp):
+    """Autocompletes a file path to the data directory."""
+    fpo = pathlib.Path(fp)
+    # Search original path first
+    if fpo.exists():
+        return fpo
+
+    # Autocomplete to the data directory
+    fpd = DP_DATA / fp
+    if fpd.exists():
+        return fpd
+
+    raise FileNotFoundError(f"The file '{fp}' does not exist and was not found under '{DP_DATA}' either.")
+
+
 def hagelhash(obj, digits:int=5) -> str:
     """Deterministically calculates a random hagelkorn-style hash of an object."""
     rng = numpy.random.RandomState(hash(obj) % (2**32 - 1))
@@ -27,7 +42,12 @@ def get_layout():
 
 
 def read_absorbances(fp) -> pandas.DataFrame:
-    df = pandas.read_csv(fp, sep=";", index_col=0)
+    """Reads absorbance measurements from TUM-style CSV layout."""
+    df = pandas.read_csv(
+        fetch_file(fp),
+        sep=";",
+        index_col=0
+    )
     df.rename(inplace=True, columns={
         c : f"{c[0]}{int(c[1:]):02d}"
         for c in df.columns
