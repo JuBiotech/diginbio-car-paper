c6e1dc546da4e528a0809e6cbda328dd3f6c36aa
Michael Osthege <m.osthege@fz-juelich.de>
2022-05-26 18:57:05 +0200

    Update chains and tune settings to values that proved to be robust

--- notebooks/pipeline.py
+++ notebooks/pipeline.py
@@ -235,8 +235,9 @@ def fit_model(wd: pathlib.Path, **sample_kwargs):
     #modelgraph.render(filename=str(wd / "model.pdf"), format="pdf")
 
     sample_kwargs.setdefault("discard_tuned_samples", False)
-    sample_kwargs.setdefault("tune", 500)
+    sample_kwargs.setdefault("tune", 1000)
     sample_kwargs.setdefault("draws", 500)
+    sample_kwargs.setdefault("chains", 6)
     # Convergence checks are computed separately
     sample_kwargs.setdefault("compute_convergence_checks", False)
 
