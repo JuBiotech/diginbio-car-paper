44c54f203172035517509d9e6649c13fb5cf8133
Michael Osthege <m.osthege@fz-juelich.de>
2022-06-01 21:59:07 +0200

    Add functions to auto-format figures in FZJ/TUM styles

--- notebooks/plotting.py
+++ notebooks/plotting.py
@@ -15,6 +15,7 @@ import sklearn.preprocessing
 import scipy
 import numpy
 import os
+import re
 import xarray
 from PIL import Image
 from typing import Any, Callable, Dict, Iterable, List, Optional, Sequence, Tuple
@@ -29,31 +30,104 @@ _log = logging.getLogger(__file__)
 pyplot.style.use(DP_ROOT / "notebooks" / "DigInBio.mplstyle")
 
 
-def apply_fzj_style(fig: pyplot.Figure):
+def _apply_to_first_letter(label: str, action) -> str:
+    """Applies a callable `action` to the first non-LaTeX command alphabetic character in a `label`."""
+    newlabel = ""
+    intex = False
+    for i,l in enumerate(label):
+        if l == "\\":
+            intex = True
+            newlabel += l
+        elif intex and l == "{":
+            intex = False
+            newlabel += l
+        elif not intex and str.isalpha(l):
+            return newlabel + action(l) + label[i+1:]
+        else:
+            newlabel += l
+    return newlabel
+
+
+def _labelformat_fzj(olabel: Optional[str]) -> str:
+    """Applies DIN 461 unit formatting and de-capitalizes the first letter."""
+    if not olabel:
+        return ""
+    label = olabel
+
+    # Find & replace
+    label = label.replace("CDW\ [g\ L^{-1}]", "Biomass\ [g_{CDW}\ L^{-1}]")
+
+    # De-capitalize first letter
+    label = _apply_to_first_letter(label, str.lower)
+    # Exceptions:
+    label = label.replace("iPTG", "IPTG")
+    label = label.replace("dO", "DO")
+
+    # Reformatting of the unit
+    label = re.sub(
+        pattern=r"\\ \[(.*?)\]",
+        repl="\ /\ {\g<1>}",
+        string=label,
+    )
+
+    _log.info("Labelconversion FZJ\n\t>>>%s<<<\n\t>>>%s<<<", olabel, label)
+    return label
+
+
+def _labelformat_tum(olabel: Optional[str]) -> str:
+    """Uses bracketed unit formatting and capitalizes the first letter."""
+    if not olabel:
+        return ""
+    label = olabel
+
+    # Capitalize first letter
+    label = _apply_to_first_letter(label, str.upper)
+    # Exceptions:
+    label = label.replace("PH", "pH")
+    label = label.replace("P(", "p(")
+
+    # NoChange: Original unit formatting is with brackes (easier to RegEx match)
+    # NoChange: Biomass vs. CDW wording (original in TUM style)
+
+    _log.info("Labelconversion TUM\n\t>>>%s<<<\n\t>>>%s<<<", olabel, label)
+    return label
+
+
+def apply_fzj_style(fig: pyplot.Figure, orig_labels):
     """Applies the following figure style:
     * First letter small
     * Unit with / notation according to DIN 461
     * No coordinate grid
     * Biomass instead of CDW
     """
+    for ax in fig.axes:
+        ylabel, xlabel, zlabel = orig_labels[ax]
+        # Disable the grid (except for 3D plots)
+        if not zlabel:
+            ax.grid(False)
+        ax.set_ylabel(_labelformat_fzj(ylabel))
+        ax.set_xlabel(_labelformat_fzj(xlabel))
+        if hasattr(ax, "set_zlabel"):
+            ax.set_zlabel(_labelformat_fzj(zlabel))
     return
 
 
-def apply_tum_style(fig: pyplot.Figure):
+def apply_tum_style(fig: pyplot.Figure, orig_labels):
     """Applies the following figure style:
     * First letter capitalized
     * Unit with [] notation
     * Coordinate grid
     * CDW instead of biomass
     """
+    for ax in fig.axes:
+        ylabel, xlabel, zlabel = orig_labels[ax]
+        # Enable the grid
+        ax.grid(True)
+        ax.set_ylabel(_labelformat_tum(ylabel))
+        ax.set_xlabel(_labelformat_tum(xlabel))
+        if hasattr(ax, "set_zlabel"):
+            ax.set_zlabel(_labelformat_tum(zlabel))
     return
-
-
-_FIGURE_STYLES = {
-    # Subfolder name : function to apply the style
-    "figs_fzj": apply_fzj_style,
-    "figs_tum": apply_tum_style,
-}
 
 
 def savefig(fig, name: str, *, wd=DP_RESULTS, **kwargs):
@@ -69,9 +143,22 @@ def savefig(fig, name: str, *, wd=DP_RESULTS, **kwargs):
     **kwargs
         Additional kwargs for `pyplot.savefig`.
     """
-    for subfolder, apply_style in _FIGURE_STYLES.items():
+    orig_labels = {}
+    for ax in fig.axes:
+        ylabel = ax.get_ylabel()
+        xlabel = ax.get_xlabel()
+        zlabel = getattr(ax, "get_zlabel", lambda: "")()
+        orig_labels[ax] = (ylabel, xlabel, zlabel)
+
+    figure_styles = {
+        # Subfolder name : function to apply the style
+        "figs_fzj": apply_fzj_style,
+        "figs_tum": apply_tum_style,
+    }
+
+    for subfolder, apply_style in figure_styles.items():
         _log.debug("Applying figure style for subfolder %s", subfolder)
-        apply_style(fig)
+        apply_style(fig, orig_labels)
         _savefig(fig, name, wd=wd / subfolder, **kwargs)
     return
 
